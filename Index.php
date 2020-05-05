f2932222ecdc4a5affa3083a77a0c47f89d6d178
sergioandresco <43445037+sergioandresco@users.noreply.github.com>
2020-05-04 22:34:09 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,34 @@
+<html lang = "es"> 
+    <header>
+        <center>
+            <h1>REGISTRO BLOCKCHAIN</h1>
+ 
+        </center>
+        
+ //formulario 
+    </header>
+    <body>
+        <form action= "envio.php" method="POST">
+            <center>
+            <label><b>Hash 1</b></label>
+            <input type= "text" name="h1" required>
+            <br><br>
+ 
+            <label><b>Hash 2</b></label>
+            <input type= "text" name="h2" required>
+            <br><br>
+ 
+            <label><b>Cantidad de dinero</b></label>
+            <input type= "text" name="dinero" required>
+            <br><br>
+ 
+            <button>ENVIAR</button>
+        
+            </center>
+ 
+        </form>
+ 
+ 
+    </body>
+ 
+</html>
